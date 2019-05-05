af1de7e2249d2de2d8c327e48a44ee59f65e4327
jsdevsandbox <jsdevsandbox@gmail.com>
2019-05-04 23:17:53 -0700

    Initial commit for the express crash course app representing simple members api with handlebars template

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+  {
+    id: 1,
+    name: "john doe",
+    email: "john@gmail.com",
+    status: "active"
+  },
+  {
+    id: 2,
+    name: "bob williams",
+    email: "bob@gmail.com",
+    status: "inactive"
+  },
+  {
+    id: 3,
+    name: "shannon jackson",
+    email: "shannon@gmail.com",
+    status: "active"
+  }
+];
+
+module.exports = members;
